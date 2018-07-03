c806f762da340807b5070d099c1a67aed8a9a4cb
ayedqarhi <ayd1400@hotmail.com>
2018-07-03 20:42:20 +0300

    add Toast to onCreate function in MainActivity

--- src/main/java/com/example/android/kotiappy/MainActivity.java
+++ src/main/java/com/example/android/kotiappy/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.kotiappy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /**
+         * Show a message when the app starts
+         */
+        Toast.makeText(this, "Hello I am Ali", Toast.LENGTH_SHORT).show();
     }
 }
